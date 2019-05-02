95a25dc0e743bbff4701297fbeba9690a9269220
Anurag <sinhaanurag18@gmail.com>
2019-05-03 01:09:28 +0530

    Revert "done some modification in class by changing its class"
    This reverts commit c50f4215e19d2ea9618703ac2c52a9453a5ae1bb.

---
+++ src/main/java/springboot/SpringmvcApplication.java
@@ -0,0 +1,17 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.controller.GreetingController;
+
+@SpringBootApplication
+public class SpringmvcApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx=SpringApplication.run(SpringmvcApplication.class, args);
+        GreetingController controller=(GreetingController)ctx.getBean("greetingController");
+        controller.sayHello();
+    }
+}
