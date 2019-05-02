91f14970f236bea4f7105e6507263282c871b64c
Anurag <sinhaanurag18@gmail.com>
2019-05-03 01:14:42 +0530

    Added @ComponentScan as in springBoot the components are only scanned in same package so by adding ComponentScan we are explicitly telling the SpringBoot container to scan a package for components

--- src/main/java/springboot/SpringmvcApplication.java
+++ src/main/java/springboot/SpringmvcApplication.java
@@ -3,10 +3,12 @@ package springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import guru.springframework.controller.GreetingController;
 
 @SpringBootApplication
+@ComponentScan("guru.springframework")
 public class SpringmvcApplication {
 
     public static void main(String[] args) {
